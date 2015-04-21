e2f1889aafacb97e0ba8140af256ee841f94f250
Karan Kumar <world12.karan@gmail.com>
2015-04-21 22:00:10 +0530

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,93 @@
+angular.module('starter.controllers', ['ngCordova'])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $cordovaGeolocation) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  $scope.getcurrentLocation= function(){
+  $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+    .then(function (position) {
+      var lat  = position.coords.latitude
+      var long = position.coords.longitude
+      alert('latitude'+ lat)
+      alert('longitude'+ long)
+    }, function(err) {
+      alert('error' +JSON.stringyfy(err))
+    });
+
+  }
+
+
+
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+
+.controller('DashBoard', function($scope){
+
+$scope.data = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+
+  $scope.addArray= function(){
+    $scope.newdata = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+    for( i=0; i < $scope.newdata.length-1; i++){
+      $scope.data.push($scope.newdata[i])
+    }
+  }
+
+})
